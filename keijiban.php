9fe9e7db12e43a8dda1cda78e4037abfcf809e86
haro99 <45450954+haro99@users.noreply.github.com>
2019-01-19 01:55:41 +0900

    Add files via upload

--- keijiban.php
+++ keijiban.php
@@ -6,21 +6,21 @@
 <title>掲示板</title>
 </head>
 <body>
-    <div class="top" style="text-align:center"><h1>掲示板</h1></div>
+    <span class="top" style="text-align:center"><h1>掲示板</h1></span>
     <?php
         //セッション開始
         session_start();
         //名前とコメントのテキストを作成
         touch("comment.txt");
         //初めて入るときに書き込み失敗や入力がありませんと表示しないように
-        if(isset($_SESSION)&&
-        isset($_POST))
+        if(isset($_SESSION['key'])&&
+        isset($_POST['key']))
         {
             //連投防止のためにセクションで管理
             if($_SESSION['key']==$_POST['key'])
             {
                 //名前とコメントが空白を確認
-                if(empty($_POST["myname"])||empty($_POST["comment"]))
+                if(empty($_POST['myname'])||empty($_POST['comment']))
                 {
                     echo "<p>入力がありません、名前とコメントを入力してください</p>";
 
@@ -29,9 +29,12 @@
                 {
                     //メモ帳の末尾に書き込む
                     $fp = fopen("comment.txt", "a");
+                    $comments = nl2br($_POST['comment']);
+                    $comments = str_replace(PHP_EOL, '', $comments);
                     //テキストに名前とコメントを一行で保存
-                    fputs($fp, $_POST["myname"]. ",". $_POST["comment"]. PHP_EOL);
+                    fputs($fp, $_POST["myname"]. ",". $comments. PHP_EOL);
                     echo "<p>". $_POST["myname"]. "さんが書き込みました</p>";
+                    echo "<p>". $comments. "</p>";
                     fclose($fp);
                 }
             }
@@ -63,7 +66,7 @@
     <div align="left">
         <form action="keijiban.php" method="POST">
         <p>名前を入力:<input type="text" name="myname"></p>
-        <p>コメント:<input type="text" name="comment"></p>
+        <p>コメント:<textarea name="comment" rows="4" cols="50" wrap="off"></textarea></p>
         <input type="hidden" name="key" value="<?= $key; ?>" />
         <input type="submit" value="OK">
         </form>
