72f4572ca7147c89136b1c2d8dd326979a1a3119
haro99 <45450954+haro99@users.noreply.github.com>
2019-02-21 17:23:31 +0900

    Add files via upload

--- keijiban.php
+++ keijiban.php
@@ -1,13 +1,4 @@
-﻿<!DOCTYPE html>
-<html laong="ja">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="sample.css">
-<title>掲示板</title>
-</head>
-<body>
-    <span class="top" style="text-align:center"><h1>掲示板</h1></span>
-    <?php
+﻿<?php
         //セッション開始
         session_start();
         //名前とコメントのテキストを作成
@@ -22,7 +13,7 @@
                 //名前とコメントが空白を確認
                 if(empty($_POST['myname'])||empty($_POST['comment']))
                 {
-                    echo "<p>入力がありません、名前とコメントを入力してください</p>";
+                    $messege = "<p>入力がありません、名前とコメントを入力してください</p>";
 
                 }
                 else
@@ -32,15 +23,15 @@
                     $comments = nl2br($_POST['comment']);
                     $comments = str_replace(PHP_EOL, '', $comments);
                     //テキストに名前とコメントを一行で保存
-                    fputs($fp, $_POST["myname"]. ",". $comments. PHP_EOL);
-                    echo "<p>". $_POST["myname"]. "さんが書き込みました</p>";
-                    echo "<p>". $comments. "</p>";
+                    fputs($fp, $_POST["number"]. ",". $_POST["myname"]. ",". $comments. PHP_EOL);
+                    $messege = "<p>". $_POST["myname"]. "さんが書き込みました</p>";
+                    //echo "<p>". $comments. "</p>";
                     fclose($fp);
                 }
             }
             else
             {
-                echo "<p>書き込みに失敗しました</p>";
+                $messege = "<p>書き込みに失敗しました</p>";
             }
         }
         // タイムスタンプと推測できない文字列にてキーを発行
@@ -48,26 +39,39 @@
         // 発行したキーをセッションに保存
         $_SESSION['key'] = $key;
     ?>
-    <ol>
+<!DOCTYPE html>
+<html laong="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="sample.css">
+    <title>掲示板</title>
+</head>
+<body>
+    <div class="top"><h1>掲示板</h1></div>
+    <?php if (isset($messege)) echo $messege; ?>
+    <div class="top-comments">
         <?php
             //コメントを一覧表示
             $deta_file = 'comment.txt';
             $ext = file_exists($deta_file);
             $lines = $ext ? file($deta_file) : array(); 
+            $number = 1;
             foreach($lines as $ln){
                 $line = explode(",", $ln);
-                echo "<li>\n";
-                echo "<p>". $line[0]. "</p>";
-                echo "<p>". $line[1]. "</p>";
-                echo "</li>\n";
+                echo "<div class=\"comments\">\n";
+                echo "<p>". $line[0]. ", ". $line[1]. "</p>";
+                echo "<p class=\"message\">". $line[2]. "</p>";
+                echo "</div>\n";
+                $number++;
             }
         ?>
-    </ol>
+    </div>
     <div align="left">
         <form action="keijiban.php" method="POST">
         <p>名前を入力:<input type="text" name="myname"></p>
         <p>コメント:<textarea name="comment" rows="4" cols="50" wrap="off"></textarea></p>
         <input type="hidden" name="key" value="<?= $key; ?>" />
+        <input type="hidden" name="number" value="<?= $number?>">
         <input type="submit" value="OK">
         </form>
     </div>
